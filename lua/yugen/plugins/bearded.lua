69cbeab1432bfddb132b69739071313fe8754c21
Gleison <gleisonmorais06@gmail.com>
2026-05-14 14:54:04 -0400

    add: new theme, bearded nvim plugin

---
+++ lua/yugen/plugins/bearded.lua
@@ -0,0 +1,21 @@
+return {
+  "Ferouk/bearded-nvim",
+  name = "bearded",
+  priority = 1000,
+  build = function()
+    -- Generate helptags so :h bearded-theme works
+    local doc = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "bearded", "doc")
+    pcall(vim.cmd, "helptags " .. doc)
+  end,
+  config = function()
+    require("bearded").setup({
+        flavor = "black-&-amethyst", -- any flavor slug
+        transparent = false,
+        bold = true,
+        italic = true,
+        terminal_colors = true,
+        on_highlights = nil,
+    })
+    vim.cmd.colorscheme("bearded")
+  end,
+}
